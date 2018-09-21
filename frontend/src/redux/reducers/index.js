6de0a99d8c4ccfe6ce857f49165a08f5f1262b4b
Felonius Gru <new-comet-dev@outlook.com>
2018-09-21 13:40:30 +0400

    define redux, thunk

---
+++ frontend/src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { postersReducer } from './posters';
+
+const reducers = combineReducers({
+    posters: postersReducer,
+});
+
+export default reducers;
