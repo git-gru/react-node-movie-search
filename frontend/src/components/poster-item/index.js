522474fe2768f96d92e79064af959d788558316e
Felonius Gru <new-comet-dev@outlook.com>
2018-09-21 16:07:32 +0400

    create poster item

---
+++ frontend/src/components/poster-item/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../build/styles/components/poster-item/style.css';
+
+const PosterItem = ({ title, url }) => (
+    <div className='poster-item'>
+        <img src={url !== "N/A" ? url : "https://www.blackfootfamilydentistry.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png"} alt='poster' />
+        <span>{title}</span>
+    </div>
+)
+
+export default PosterItem;
