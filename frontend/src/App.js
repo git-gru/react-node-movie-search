e165e1b10879b1a02572f3cab614b847fd28de14
Felonius Gru <new-comet-dev@outlook.com>
2018-09-21 16:07:10 +0400

    render posters container for root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import PostersListContainer from './containers/posters-list';
+
+const App = ({ store }) => (
+    <Provider store={store}>
+        <Router>
+            <Route path="/" component={PostersListContainer} />
+        </Router>
+    </Provider>
+);
 
 export default App;
---
+++ frontend/src/containers/posters-list/index.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import PostersList from '../../components/posters-list';
+import { getPosters } from '../../redux/actions/posters';
+
+const mapStateToProps = store => ({
+    posters: store.posters.posters,
+});
+
+const mapActionToProps = {
+    getPosters,
+};
+
+const PostersListContainer = connect(mapStateToProps, mapActionToProps)(PostersList);
+export default PostersListContainer;
