b91be2c7367337aa699601ca8e870315297507fe
GRU <new-comet-dev@outlook.com>
2022-03-27 09:06:04 -0700

    update frontend project from create-react-app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { configureStore } from './redux/store';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { configureStore } from "./redux/store";
 
 const store = configureStore();
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App store={store} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
